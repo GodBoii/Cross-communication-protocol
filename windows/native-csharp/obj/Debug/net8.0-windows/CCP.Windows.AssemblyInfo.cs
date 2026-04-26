d7f3cb8a5cac8a028bbe059eb6dcb2defcbd99d7
Prajwal Ghadge <prajwalghadge2005@gmail.com>
2026-04-26 13:07:20 +0530

    Update generated app code and assembly info

--- windows/native-csharp/obj/Debug/net8.0-windows/CCP.Windows.AssemblyInfo.cs
+++ windows/native-csharp/obj/Debug/net8.0-windows/CCP.Windows.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCP.Windows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32e2c3abe8ccc33717b575112733d33b7cca8bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92f3584f8f44fdf2f44cd7f381671849a61aa9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCP.Windows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCP.Windows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
